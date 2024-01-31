c7c417a750add04dc198db2a852eada518c22e1d
Aurel HEBERT-BURGGRAEVE <ahebertb@IFREMER.FR>
2024-01-31 17:21:22 +0100

    maj url

--- R/mod_reception.R
+++ R/mod_reception.R
@@ -16,6 +16,11 @@ mod_reception_ui <- function(id){
     textOutput(ns("author")),
     br(),
     actionButton(ns("guide"), "Guide utilisateur", icon = icon("book")),
+    uiOutput(ns("ref_file_url_1")),
+    uiOutput(ns("ref_file_url_2")),
+    uiOutput(ns("ref_file_url_3")),
+    uiOutput(ns("ref_file_url_4")),
+    #uiOutput(ns("ref_file_url_5")),
     hr(),
     actionButton("start", "start", icon = icon("ship"))
     )
@@ -32,6 +37,27 @@ mod_reception_server <- function(input, output, session, r){
       "developed by Aurel Hebert--Burggraeve, Mathis Cambreling, Jehanne Rivet,
       Laure Simplet, Vincent Badts, Laurent Dubroca, Camille Vogel"
     })
+
+    output$ref_file_url_1 <- renderUI({
+      url <- a("WGEXT", href="https://ices-library.figshare.com/articles/report/Working_Group_on_the_Effects_of_Extraction_of_Marine_Sediments_on_the_Marine_Ecosystem_WGEXT_/18621728/1")
+      tagList("URL link: ", url)
+    })
+    output$ref_file_url_2 <- renderUI({
+      url <- a("DOGGM", href="https://side.developpement-durable.gouv.fr/PACA/doc/SYRACUSE/385733/guide-methodologique-pour-l-elaboration-des-documents-d-orientations-pour-une-gestion-durable-des-gr")
+      tagList("URL link: ", url)
+    })
+    output$ref_file_url_3 <- renderUI({
+      url <- a("Protocole_halieutique", href="https://www.geo-ocean.fr/Expertise/Appui-a-la-Puissance-Publique/Les-granulats-marins/Granulats-marins/Protocoles/Ressources-halieutiques")
+      tagList("URL link: ", url)
+    })
+    output$ref_file_url_4 <- renderUI({
+      url <- a("Guide_technique", href="https://www.mineralinfo.fr/fr/actualite/actualite/elaboration-des-etudes-dimpact-granulats-marins-guide-technique-2023")
+      tagList("URL link: ", url)
+    })
+    #output$ref_file_url_5 <- renderUI({
+    #  url <- a("", href="")
+    #  tagList("URL link: ", url)
+    #})
 
 }
 
